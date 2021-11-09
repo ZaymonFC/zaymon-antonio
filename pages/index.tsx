fdf487d29ce1eb7e1fe17c2750ac228454f33851
Zaymon Foulds-Cook <12402727+ZaymonFC@users.noreply.github.com>
2021-11-09 15:08:36 +1000

    ✨ Factor out boot screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,63 +26,8 @@ const Main = styled("main", {
   padding: "32px",
 });
 
-const border = {
-  borderWidth: "2px",
-  borderColor: "#42bbf3",
-  borderStyle: "solid",
-  borderRadius: "2px",
-};
-
-const Dialogue = styled("fieldset", {
-  ...border,
-
-  fontFamily: "Jetbrains Mono",
-  fontSize: "1rem",
-  backgroundColor: "#00000044",
-  color: "White",
-
-  marginBottom: "16px",
-
-  boxShadow: `
-    1.7px 1.7px 3.5px rgba(0, 0, 0, 0.019),
-    3.9px 3.9px 7.8px rgba(0, 0, 0, 0.03),
-    6.7px 6.7px 13.2px rgba(0, 0, 0, 0.039),
-    10.3px 10.3px 20.4px rgba(0, 0, 0, 0.046),
-    15.2px 15.2px 30.2px rgba(0, 0, 0, 0.053),
-    22.3px 22.3px 44.4px rgba(0, 0, 0, 0.059),
-    33.5px 33.5px 66.6px rgba(0, 0, 0, 0.064),
-    53.4px 53.4px 106.2px rgba(0, 0, 0, 0.068),
-    100px 100px 199px rgba(0, 0, 0, 0.07)
-  `,
-
-  hover: {
-    color: "Green",
-  },
-
-  maxWidth: "320px",
-});
-
-const Legend = styled("legend", {
-  color: "White",
-
-  fontSize: "2rem",
-  fontFamily: "Cardo",
-  padding: "0 0.5rem",
-  borderRadius: "4px",
-});
-
-const SelectItem = styled("a", {
-  padding: "4px",
-  borderRadius: "2px",
-  "&:hover": {
-    cursor: "pointer",
-    backgroundColor: "#386aa3",
-  },
-});
-
 const Heading = styled("h1", {
   margin: 0,
-
   fontFamily: "Cardo",
   fontSize: "4rem",
   color: "white",
@@ -115,6 +60,42 @@ const HomeMarkerCardBody = styled(MarkerCardBody, {
   fontSize: 20,
 });
 
+const BootScreen = ({ setBooted }: { setBooted: () => void }) => (
+  <Fade duration={0.3}>
+    <CenterScreen>
+      <HomeMarkerCard>
+        <MarkerCardHeading>Zaymon.dev</MarkerCardHeading>
+        <HomeMarkerCardBody>
+          Welcome.
+          <br />
+          <br />
+          <MarkerCardButton onClick={() => setBooted()}>
+            Press to [B]oot the Universe
+          </MarkerCardButton>
+        </HomeMarkerCardBody>
+      </HomeMarkerCard>
+    </CenterScreen>
+  </Fade>
+);
+
+const TheGalaxy = () => {
+  return (
+    <>
+      <Three />
+      <Main>
+        <Fade duration={0.5} delay={0.5}>
+          <Heading>Zaymon Antonio.</Heading>
+        </Fade>
+        <Fade duration={0.5} delay={0.8}>
+          <TagLine>Software Engineer.</TagLine>
+        </Fade>
+
+        <div style={{ marginTop: "48px" }}></div>
+      </Main>
+    </>
+  );
+};
+
 const Home: NextPage = () => {
   const [booted, setBooted] = React.useState(false);
 
@@ -133,38 +114,8 @@ const Home: NextPage = () => {
       </Head>
 
       <Body>
-        {!booted && (
-          <Fade duration={0.3}>
-            <CenterScreen>
-              <HomeMarkerCard>
-                <MarkerCardHeading>Zaymon.dev</MarkerCardHeading>
-                <HomeMarkerCardBody>
-                  Welcome.
-                  <br />
-                  <br />
-                  <MarkerCardButton onClick={() => setBooted(true)}>
-                    Press to [B]oot the Universe
-                  </MarkerCardButton>
-                </HomeMarkerCardBody>
-              </HomeMarkerCard>
-            </CenterScreen>
-          </Fade>
-        )}
-        {booted && (
-          <>
-            <Three />
-            <Main>
-              <Fade duration={0.5} delay={0.5}>
-                <Heading>Zaymon Antonio.</Heading>
-              </Fade>
-              <Fade duration={0.5} delay={0.8}>
-                <TagLine>Software Engineer.</TagLine>
-              </Fade>
-
-              <div style={{ marginTop: "48px" }}></div>
-            </Main>
-          </>
-        )}
+        {!booted && <BootScreen setBooted={() => setBooted(true)} />}
+        {booted && <TheGalaxy />}
       </Body>
     </>
   );
