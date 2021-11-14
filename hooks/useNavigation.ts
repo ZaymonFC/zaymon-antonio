50c049a11275b1ae47c4269356f25fa2b3564bda
Zaymon Foulds-Cook <12402727+ZaymonFC@users.noreply.github.com>
2021-11-14 10:43:46 +1000

    🪝 hook: useNavigation

---
+++ hooks/useNavigation.ts
@@ -0,0 +1,33 @@
+import create from "zustand";
+
+const initialNavigationState = {
+  current: 0,
+  points: {} as { [rank: number]: [number, number] },
+};
+
+type Direction = "Left" | "Right";
+
+const calculateMove = (n: number, current: number, direction: Direction) => {
+  switch (direction) {
+    case "Left":
+      return (current + 1) % n;
+    case "Right":
+      return current === 0 ? n - 1 : current - 1;
+  }
+};
+
+type NavigationState = typeof initialNavigationState & {
+  recordPoint: (rank: number, point: [number, number]) => void;
+  move: (dir: Direction) => void;
+};
+
+export const useNavigation = create<NavigationState>((set) => ({
+  ...initialNavigationState,
+  recordPoint: (rank, point) =>
+    set((state) => ({ ...state, points: { ...state.points, [rank]: point } })),
+  move: (dir) =>
+    set((state) => ({
+      ...state,
+      current: calculateMove(Object.keys(state.points).length, state.current, dir),
+    })),
+}));
